d77399ad7de3104c2c5cbdb9e50f54e0702c5dc5
A540 <51090700+A540@users.noreply.github.com>
2024-03-27 17:46:40 +0900

    Django course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from accountapp.models import HelloWorld
 
@@ -16,7 +19,13 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) # reverse함수는 def에서 사용가능
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
