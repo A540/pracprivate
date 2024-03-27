29cc5a6ac308cc1fba56411f7952b87f5308bc43
A540 <51090700+A540@users.noreply.github.com>
2024-03-27 18:41:45 +0900

    Django course 22 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from accountapp.views import hello_world, AccountCreateView
 
@@ -6,5 +7,8 @@ app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- test123/settings.py
+++ test123/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import environ
 import os
+from django.urls import reverse_lazy
 
 from django.contrib import staticfiles
 from django.template.context_processors import static
@@ -136,3 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
