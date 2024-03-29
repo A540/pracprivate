ba8f4ad644da16a0a07b973e6794c276b76f994b
A540 <51090700+A540@users.noreply.github.com>
2024-03-29 19:38:43 +0900

    Django course 28 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import PasswordChangeView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+has_ownership = [account_ownership_required, login_required]
+
 # Create your views here.
+@login_required
 def hello_world(request):
     if request.method == 'POST':
 
@@ -21,7 +27,7 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world')) # reverse함수는 def에서 사용가능
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # reverse함수는 def에서 사용가능
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
@@ -40,14 +46,18 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
-class AccountUpdateView(PasswordChangeView):    # Updateview 사용시 username 변경이 필수로 바뀜
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(PasswordChangeView):  # Updateview 사용시 username 변경이 필수로 바뀜
     model = User
-    context_object_name = 'target_user'
+    # context_object_name = 'user'
     # form_class = AccountUpdateForm            # Updateview 사용시 주석 해제
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
