18eb33d024ce6e1eb5b3add37328044e9bd295e2
A540 <51090700+A540@users.noreply.github.com>
2024-03-29 18:53:22 +0900

    Django course 26 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -26,19 +26,28 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
 
 class AccountUpdateView(PasswordChangeView):    # Updateview 사용시 username 변경이 필수로 바뀜
     model = User
     # form_class = AccountUpdateForm            # Updateview 사용시 주석 해제
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
