d0f3551b99e08ee3ea709474a2ad17d1d8233f64
A540 <51090700+A540@users.noreply.github.com>
2024-04-01 18:58:44 +0900

    Django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # ↓ profile 변경후 detail페이지로 가기위한 pk값을 넘겨줄 수 없음 -> 밑에 get_success_url함수의 kwargs로 넘겨주기
+    # success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
     
     def form_valid(self, form):
@@ -23,6 +24,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -30,5 +34,9 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # ↓ profile 변경후 detail페이지로 가기위한 pk값을 넘겨줄 수 없음 -> 밑에 get_success_url함수의 kwargs로 넘겨주기
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
