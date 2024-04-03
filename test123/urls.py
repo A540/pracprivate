079891d59f4971766184d76e3212058f25307d08
A540 <51090700+A540@users.noreply.github.com>
2024-04-03 20:03:43 +0900

    Django course 37 commit

--- test123/urls.py
+++ test123/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls')),
+    path('account/', include('accountapp.urls')),
     path('profile/', include('profileapp.urls')),
     path('article/', include('articleapp.urls')),
+    path('comment/', include('commentapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
