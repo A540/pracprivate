435fba2146480282cce61b2b77c7a39a32ddcf01
A540 <51090700+A540@users.noreply.github.com>
2024-04-15 16:45:10 +0900

    Django course 41 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
+
 # Create your models here.
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='article')
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name='project')
+
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='articles/', null=False)
     content = models.TextField(null=True)
